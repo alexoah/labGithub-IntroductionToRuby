4ca17c212ec56b5a76060d18fe836eaf9c742725
Alexoah YT <alexandra.soah@gmail.com>
2020-02-19 16:09:38 +0700

    Modified Else statement and added case insensitive checking

--- montys-color.rb
+++ montys-color.rb
@@ -3,10 +3,12 @@ puts "What is your favorite color?"
 color = gets.chop
 
 # Lesson 4: Using an If Statement
-if color == "pink"
-    puts "Wow! My favorite color is #{color} also!"
+favColor = "blue"
+favAltColor = "green"
+if color.downcase == "red"
+    puts "It's not as good as #{favColor} but #{color} isn't too bad."
 # Modifying If Statement from Lesson 5 to ElsIf
-elsif color == "blue"
+elsif (color.downcase == favColor) || (color.downcase == favAltColor)
     puts "Wow! My favorite color is #{color} also!"
 # Lesson 5: Adding an Else Statement
 else
