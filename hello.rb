ad93ca7de9acbf8f11e675f635fa0f38901e0315
Alexoah YT <alexandra.soah@gmail.com>
2020-02-19 15:51:22 +0700

    Added some Ruby methods

--- hello.rb
+++ hello.rb
@@ -1,8 +1,8 @@
 # Lesson 1: Outputting a String
-
 puts "Hello World!"
 
 # Lesson 2: Prompting User Input
-
-# name = gets
-# puts "Hey, #{name}! Nice to meet you!"
+# gets will include newline character, 
+# to make it get the input without newline character, use gets.chop
+name = gets.chop 
+puts "Hey, #{name}! Nice to meet you!"
